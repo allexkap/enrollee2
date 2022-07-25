5a569b43d4ce078974d75d35160781c8b3631965
allexkap <aekpstn@gmail.com>
2022-07-25 16:52:28 +0300

    add count method

--- parse.py
+++ parse.py
@@ -1,10 +1,13 @@
 class Handler:
 
-    def __init__(self, scores):
+
+    def __init__(self, parser, **scores):
+        self.parser = parser
         self.scores = scores
+        self.page = 0
+
 
     def compare(self, other):
-
         total = 0
         order = True
         equality = True
@@ -16,5 +19,21 @@ class Handler:
                 order = False
             if equality and total != 0:
                 equality = False
-
+        if equality: print('=')
         return total > 0 or order and not equality
+
+
+    def __call__(self):
+        self.data = {'bvi': 0, 'ege': 0, 'sgl': 0}
+        self.metadata = None
+
+        for data, score in self.parser(self.page):
+            if 'total' in data:
+                self.metadata = data
+            elif data['bvi']:
+                self.data['bvi'] += 1
+            elif not self.compare(score):
+                if data['sgl']:
+                    self.data['sgl'] += 1
+                else:
+                    self.data['ege'] += 1
